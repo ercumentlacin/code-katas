d081827f78be25d3b700817a594fe74ba87165b8
ercumentlacin <ercument.lacin@akinon.com>
2022-02-25 20:27:43 +0300

    Add Build Tower

---
+++ Build_Tower/builder-tower.js
@@ -0,0 +1,22 @@
+function towerBuilder(nFloors) {
+  const tower = [];
+  const asteriks = '*';
+  const space = ' ';
+
+  // her sayi için bir n oluşturuyoruz
+  for (let n = 1; n <= nFloors; n++) {
+    // tower dizisinin her indeksine (n*2-1) kadar yildiz ekliyoruz ve bu yildizlarin basina ve sonuna bosluk ekliyoruz bosluk sayisina space diyelim
+    // space = nFloors - i
+    const count = n * 2 - 1 < 0 ? 1 : n * 2 - 1;
+    const spaceCount = nFloors - n;
+    const spaceWrite = space.repeat(spaceCount);
+    const floor = spaceWrite + asteriks.repeat(count) + spaceWrite;
+    tower.push(floor);
+  }
+
+  return tower;
+}
+
+console.log(towerBuilder(1), ['*']);
+console.log(towerBuilder(2), [' * ', '***']);
+console.log(towerBuilder(3), ['  *  ', ' *** ', '*****']);
--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 
 | Kata Name   | Code                                  | Description Link                                                          | Kyu |
 | ----------- | ------------------------------------- | ------------------------------------------------------------------------- | --- |
-| Build Tower | [Link](/Build_Tower/builder-tower.js) | [Link](https://www.codewars.com/kata/576757b1df89ecf5bd00073b/javascript) | 5   |
+| Build Tower | [Link](/Build_Tower/builder-tower.js) | [Link](https://www.codewars.com/kata/576757b1df89ecf5bd00073b/javascript) | 6   |
